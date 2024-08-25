e32676092a774e270071984820eba5aa7a9471b6
alarrabee <alexlarrabee10@gmail.com>
2024-08-24 22:27:34 -0500

    Inserted five assorted images for project layout and design enhancement

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,21 +1,22 @@
-import Header from './components/header'
-import Footer from './components/footer'
+import ComputerImg from './assets/images/computer.png'
+import BookImg from './assets/images/books.png'
+import DragonImg from './assets/images/dragon.png'
+import PlantImg from './assets/images/plants.png'
+import TeaImg from './assets/images/tea.png'
 
 function App() {
     return (
-      <div className='parent' style={{ border: '2px solid black' }}>
-        <div className='div1' style={{ border: '2px solid black' }}><Header /></div>
-        <div className='div2' style={{ border: '2px solid black' }}>header image here</div>
-        <div className='div3' style={{ border: '2px solid black' }}>about section</div>
-        <div className='div4' style={{ border: '2px solid black' }}>image one</div>
-        <div className='div5' style={{ border: '2px solid black' }}>image two</div>
-        <div className='div6' style={{ border: '2px solid black' }}>skills</div>
-        <div className='div7' style={{ border: '2px solid black' }}>timeline component</div>
-        <div className='div8' style={{ border: '2px solid black' }}>image three</div>
-        <div className='div9' style={{ border: '2px solid black' }}>image four</div>
-        <div className='div10' style={{ border: '2px solid black' }}><Footer /></div>
-      </div>
+        <div class="parent">
+            <div class="div1"><img src={ComputerImg} alt='cozy computer desk set in front of a window surrounded by plants' /></div>
+            <div class="div2" style={{ border: '2px solid black' }}>About</div>
+            <div class="div3" style={{ border: '2px solid black' }}><img src={BookImg} alt='books on a wooden shelf' /></div>
+            <div class="div4" style={{ border: '2px solid black' }}><img src={DragonImg} alt='green dragon curled up on a pillow' /></div>
+            <div class="div5" style={{ border: '2px solid black' }}>Skills</div>
+            <div class="div6" style={{ border: '2px solid black' }}>Timeline</div>
+            <div class="div7" style={{ border: '2px solid black' }}><img src={PlantImg} alt='potted plants arranged on a windowsill' /></div>
+            <div class="div8" style={{ border: '2px solid black' }}><img src={TeaImg} alt='cup of tea on a small wooden table' /></div>
+        </div>
     )
-    }
+}
   
-  export default App
+export default App
