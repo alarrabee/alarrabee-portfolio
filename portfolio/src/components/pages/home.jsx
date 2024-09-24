d6c0209de7cebaba92b37c9384d779c8f214bf63
alarrabee <alexlarrabee10@gmail.com>
2024-09-23 21:21:38 -0500

    add green tea png to home page

--- portfolio/src/components/pages/home.jsx
+++ portfolio/src/components/pages/home.jsx
@@ -1,5 +1,6 @@
 import './home.css'
 import { useNavigate } from 'react-router-dom';
+import greenTea from '../../assets/images/green-tea.png'
 
 const Home = () => {
     const navigate = useNavigate();
@@ -10,14 +11,18 @@ const Home = () => {
 
   return (
     <body>
-      <div className="title-home">
+      <div className="home-title-container">
+        <div className="tea-image-container">
+            <img className="tea-img" src={greenTea} alt="Green tea"/>
+        </div>
+
         <h1 className='text-with-cursor'>Hello Traveler. Glad you stopped by...</h1>
         <p>My name is Alex. This portfolio is best enjoyed with a warm cup of tea.  <span className='cursor'></span></p>
 
         <button onClick={handleButtonClick}>
             Go to About Page
         </button>
-        
+
       </div>
     </body>
   )
