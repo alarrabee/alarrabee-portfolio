632f2c0a1ce1e4e298a855720352118851e0e4f1
alarrabee <alexlarrabee10@gmail.com>
2024-09-23 20:02:46 -0500

    add home, about, portfolio, resume, and error pages; set up routes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,21 +1,24 @@
-import ComputerImg from './assets/images/computer.png'
-import BookImg from './assets/images/books.png'
-import DragonImg from './assets/images/dragon.png'
-import PlantImg from './assets/images/plants.png'
-import TeaImg from './assets/images/tea.png'
-import About from './components/about'
+import { Outlet } from 'react-router-dom'
+
+// import ComputerImg from './assets/images/computer.png'
+// import BookImg from './assets/images/books.png'
+// import DragonImg from './assets/images/dragon.png'
+// import PlantImg from './assets/images/plants.png'
+// import TeaImg from './assets/images/tea.png'
+// import About from './components/about'
 
 function App() {
     return (
         <div class="parent">
-            <div class="div1"><img src={ComputerImg} alt='cozy computer desk set in front of a window surrounded by plants' /></div>
+            {/* <div class="div1"><img src={ComputerImg} alt='cozy computer desk set in front of a window surrounded by plants' /></div>
             <div class="div2" style={{ border: '2px solid black' }}><About /></div>
             <div class="div3" style={{ border: '2px solid black' }}><img src={BookImg} alt='books on a wooden shelf' /></div>
             <div class="div4" style={{ border: '2px solid black' }}><img src={DragonImg} alt='green dragon curled up on a pillow' /></div>
             <div class="div5" style={{ border: '2px solid black' }}>Skills</div>
             <div class="div6" style={{ border: '2px solid black' }}>Timeline</div>
             <div class="div7" style={{ border: '2px solid black' }}><img src={PlantImg} alt='potted plants arranged on a windowsill' /></div>
-            <div class="div8" style={{ border: '2px solid black' }}><img src={TeaImg} alt='cup of tea on a small wooden table' /></div>
+            <div class="div8" style={{ border: '2px solid black' }}><img src={TeaImg} alt='cup of tea on a small wooden table' /></div> */}
+            <Outlet />
         </div>
     )
 }
--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx'
-import './index.css'
+import Home from './components/pages/home.jsx'
+import About from './components/pages/about.jsx'
+import Portfolio from './components/pages/portfolio.jsx'
+import Resume from './components/pages/resume.jsx'
+import ErrorPage from './components/pages/error.jsx'
+
+//defined routes
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path:'about',
+          element: <About/>
+        },
+        {
+          path:'portfolio',
+          element: <Portfolio/>
+        },
+        {
+          path:'contact',
+          element: <Resume/>
+        }
+      ],
+    },
+  ]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 )
