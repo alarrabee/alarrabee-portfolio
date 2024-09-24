b60ae81fa65567307d896ec66064dcaa2d1be1cc
alarrabee <alexlarrabee10@gmail.com>
2024-09-23 20:47:27 -0500

    add home page styles; add button on home page to navigate to the about page

--- portfolio/src/components/pages/home.jsx
+++ portfolio/src/components/pages/home.jsx
@@ -1,7 +1,26 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
-}
+import './home.css'
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    const navigate = useNavigate();
+    const handleButtonClick = () => {
+        navigate('/about');
+    }
+
+
+  return (
+    <body>
+      <div className="title-home">
+        <h1 className='text-with-cursor'>Hello Traveler. Glad you stopped by...</h1>
+        <p>My name is Alex. This portfolio is best enjoyed with a warm cup of tea.  <span className='cursor'></span></p>
+
+        <button onClick={handleButtonClick}>
+            Go to About Page
+        </button>
+        
+      </div>
+    </body>
+  )
+};
+
+export default Home;
